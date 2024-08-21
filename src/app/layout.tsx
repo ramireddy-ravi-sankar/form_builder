7058755af8f96702afcd0c7fbe322d3d82a7e9a0
Ramireddy Ravi Sankar Reddy <ravisankar.ramireddy@artifixel.com>
2024-08-21 17:06:43 +0530

    Form Builder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import DesignerContextProvider from "@/components/context/DesignerContext";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +20,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider publishableKey="pk_test_ZXZpZGVudC1mbG91bmRlci01LmNsZXJrLmFjY291bnRzLmRldiQ">
+        <html lang="en">
+        <body className={inter.className}>
+         <DesignerContextProvider>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+            {children}
+            <Toaster />
+            </ThemeProvider>
+         </DesignerContextProvider> 
+        
+        </body>
+        </html>
+    </ClerkProvider>
+    
   );
 }
---
+++ src/components/FormBuilder.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { Form } from '@prisma/client'
+import React from 'react'
+import PreviewDialogBtn from './PreviewDialogBtn';
+import SaveFormBtn from './SaveFormBtn';
+import PublishFormBtn from './PublishFormBtn';
+import Designer from './Designer';
+import {DndContext, useSensors, useSensor, TouchSensor, MouseSensor, } from "@dnd-kit/core";
+import DragOverlayWrapper from './DragOverlayWrapper';
+
+
+
+function FormBuilder({form}:{form:Form}) {
+
+    const mouseSensor=useSensor(MouseSensor,{
+        activationConstraint:{
+            distance: 10, //10px
+        },
+    });
+
+    const touchSensor=useSensor(TouchSensor,{
+        activationConstraint:{
+            delay: 300, 
+            tolerance: 5,
+        },
+    })
+
+    const sensors=useSensors(mouseSensor, touchSensor);
+
+  return (
+    <DndContext sensors={sensors}>
+        <main className='flex flex-col w-full'>
+        <nav className='flex justify-between border-b-2 p-4 gap-3 items-center'>
+            <h2 className='truncate font-medium'>
+                <span className='text-muted-foreground mr-2'>Form:</span>
+                {form.name}
+            </h2>
+            <div className="flex items-center gap-2">
+                <PreviewDialogBtn />
+
+                {!form.published && (
+                    <>
+                        <SaveFormBtn />
+                        <PublishFormBtn />
+                    </>
+
+                )}
+            </div>
+        </nav>
+        <div className='flex w-full flex-grow items-center justify-center 
+        relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)]
+        dark:bg-[url(/paper-dark.svg)]'>
+            <Designer />
+        </div>
+    </main>
+    <DragOverlayWrapper />
+    </DndContext>
+  );
+}
+
+export default FormBuilder
